36448c1c3d08084477250d7bad7928fc046d71cb
Dohyeon Park <dohun94@naver.com>
2020-05-25 01:15:52 +0900

    #122 Best Time to Buy and Sell Stock 2

---
+++ leetCodeJava/src/com/leetcode/blackdog/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,34 @@
+package com.leetcode.blackdog;
+
+public class BestTimeToBuyAndSellStock2 {
+
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock2 bestTimeToBuyAndSellStock2 = new BestTimeToBuyAndSellStock2();
+
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        System.out.println(bestTimeToBuyAndSellStock2.maxProfit(prices));
+    }
+
+    public int maxProfit(int[] prices) {
+        return calculate(prices, 0);
+    }
+
+    public int calculate(int prices[], int s) {
+        if (s >= prices.length)
+            return 0;
+        int max = 0;
+        for (int start = s; start < prices.length; start++) {
+            int maxprofit = 0;
+            for (int i = start + 1; i < prices.length; i++) {
+                if (prices[start] < prices[i]) {
+                    int profit = calculate(prices, i + 1) + prices[i] - prices[start];
+                    if (profit > maxprofit)
+                        maxprofit = profit;
+                }
+            }
+            if (maxprofit > max)
+                max = maxprofit;
+        }
+        return max;
+    }
+}
